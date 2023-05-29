d33bec261097f727e85ae7c16df718f4b6db8da6
Adrian Roman <aroman_ro@yahoo.com>
2023-05-29 09:56:14 +0300

    Refactoring a little the Irist neural network example code

--- MachineLearning/MachineLearning/NeuralNetworkTestsIris.cpp
+++ MachineLearning/MachineLearning/NeuralNetworkTestsIris.cpp
@@ -6,22 +6,8 @@
 #include "MNISTDatabase.h"
 #include "Softmax.h"
 
-
-bool IrisNeuralNetworkTest()
+void ShuffleIris(std::vector<Utils::IrisDataset::Record>& records, int nrTraining)
 {
-	std::cout << std::endl << "Neural Network test for the Iris dataset, Setosa is lineary separable from the other two, but the others two cannot be linearly separated" << std::endl << std::endl;
-
-	Utils::IrisDataset irisDataset;
-	irisDataset.setRelativePath("../../Datasets/");
-	irisDataset.setDataFileName("iris.data");
-
-	if (!irisDataset.Open()) return false;
-
-	auto records = irisDataset.getAllRecords();
-	const int nrTraining = 120;
-
-	// shuffle the data
-
 	std::random_device rd;
 	std::mt19937 g(rd());
 
@@ -46,20 +32,13 @@ bool IrisNeuralNetworkTest()
 
 		if (setosa > 8 && versicolor > 8 && virginica > 8) break;
 	}
+}
 
-
-	//for (auto rec : records)
-	//	std::cout << std::get<0>(rec) << ", " << std::get<1>(rec) << ", " << std::get<2>(rec) << ", " << std::get<3>(rec) << ", " << std::get<4>(rec) << std::endl;
-
-	// split the data into training and test sets
-
-	std::vector<Utils::IrisDataset::Record> trainingSet(records.begin(), records.begin() + nrTraining);
-	std::vector<Utils::IrisDataset::Record> testSet(records.begin() + nrTraining, records.end());
-
-	// create the model
-	const int nrOutputs = 3; // 1 only for Setosa, 3 if all three classes are to be predicted
-
+void NormalizeIris(std::vector<Utils::IrisDataset::Record>& trainingSet, std::vector<Utils::IrisDataset::Record>& testSet, int nrOutputs = 3)
+{
 	// normalize the inputs
+	const int nrTraining = static_cast<int>(trainingSet.size());
+
 	Norm::Normalizer normalizer(4, nrOutputs);
 	Eigen::MatrixXd x(4, nrTraining);
 	Eigen::MatrixXd y(nrOutputs, nrTraining);
@@ -103,6 +82,86 @@ bool IrisNeuralNetworkTest()
 
 		testSet[i] = std::make_tuple(x(0, 0), x(1, 0), x(2, 0), x(3, 0), std::get<4>(testSet[i]));
 	}
+}
+
+template<class NeuralNetworkClass> void PrintStats(NeuralNetworkClass& neuralNetwork, const std::vector<Utils::IrisDataset::Record>& records, int batchSize, int nrOutputs = 3)
+{
+	Eigen::MatrixXd in(4, batchSize);
+	Eigen::MatrixXd out(nrOutputs, batchSize);
+
+	Utils::TestStatistics setosaStats;
+	Utils::TestStatistics versicolorStats;
+	Utils::TestStatistics virginicaStats;
+
+	long long int correct = 0;
+	for (const auto& record : records)
+	{
+		in(0, 0) = std::get<0>(record);
+		in(1, 0) = std::get<1>(record);
+		in(2, 0) = std::get<2>(record);
+		in(3, 0) = std::get<3>(record);
+
+		out(0, 0) = (std::get<4>(record) == "Iris-setosa") ? 1 : 0;
+		if (nrOutputs > 1) out(1, 0) = (std::get<4>(record) == "Iris-versicolor") ? 1 : 0;
+		if (nrOutputs > 2) out(2, 0) = (std::get<4>(record) == "Iris-virginica") ? 1 : 0;
+
+		Eigen::VectorXd res = neuralNetwork.Predict(in.col(0));
+
+
+		setosaStats.AddPrediction(res(0) > 0.5, out(0, 0) > 0.5);
+		if (nrOutputs > 1) versicolorStats.AddPrediction(res(1) > 0.5, out(1, 0) > 0.5);
+		if (nrOutputs > 2) virginicaStats.AddPrediction(res(2) > 0.5, out(2, 0) > 0.5);
+
+		double limp = 0.5;
+		for (int j = 0; j < nrOutputs; ++j)
+			limp = std::max(limp, res(j));
+
+		if (res(0) == limp && out(0, 0) > 0.5) ++correct;
+		else if (nrOutputs > 1 && res(1) == limp && out(1, 0) > 0.5) ++correct;
+		else if (nrOutputs > 2 && res(2) == limp && out(2, 0) > 0.5) ++correct;
+	}
+
+	setosaStats.PrintStatistics("Setosa");
+	if (nrOutputs > 1) {
+		versicolorStats.PrintStatistics("Versicolor");
+		if (nrOutputs > 2) virginicaStats.PrintStatistics("Virginica");
+	}
+
+
+	std::cout << "Accuracy (% correct): " << 100.0 * static_cast<double>(correct) / static_cast<double>(records.size()) << "%" << std::endl << std::endl;
+}
+
+
+bool IrisNeuralNetworkTest()
+{
+	std::cout << std::endl << "Neural Network test for the Iris dataset, Setosa is lineary separable from the other two, but the others two cannot be linearly separated" << std::endl << std::endl;
+
+	Utils::IrisDataset irisDataset;
+	irisDataset.setRelativePath("../../Datasets/");
+	irisDataset.setDataFileName("iris.data");
+
+	if (!irisDataset.Open()) return false;
+
+	auto records = irisDataset.getAllRecords();
+	const int nrTraining = 120;
+
+	// shuffle the data
+
+	ShuffleIris(records, nrTraining);
+
+
+	// split the data into training and test sets
+
+	std::vector<Utils::IrisDataset::Record> trainingSet(records.begin(), records.begin() + nrTraining);
+	std::vector<Utils::IrisDataset::Record> testSet(records.begin() + nrTraining, records.end());
+
+
+	const int nrOutputs = 3; // 1 only for Setosa, 3 if all three classes are to be predicted
+
+	// normalize the inputs
+	NormalizeIris(trainingSet, testSet, nrOutputs);
+
+	// create the model
 
 	// more layers can be added, and/or made wider, but it will take more time to train. One configuration that I tried: { 4, 246, 512, 127, 63, 27, 9, nrOutputs }
 	NeuralNetworks::MultilayerPerceptron<SGD::SoftmaxRegressionAdamSolver> neuralNetwork({ 4, 27, 9, nrOutputs });
@@ -151,111 +210,15 @@ bool IrisNeuralNetworkTest()
 		}
 	}
 
-
-	Utils::TestStatistics setosaStats;
-	Utils::TestStatistics versicolorStats;
-	Utils::TestStatistics virginicaStats;
-
-
 	// test the model
-
-	// first, on training set:
 
 	std::cout << std::endl << "Training set:" << std::endl;
 
-	long long int correct = 0;
-	for (const auto& record : trainingSet)
-	{
-		in(0, 0) = std::get<0>(record);
-		in(1, 0) = std::get<1>(record);
-		in(2, 0) = std::get<2>(record);
-		in(3, 0) = std::get<3>(record);
-
-		out(0, 0) = (std::get<4>(record) == "Iris-setosa") ? 1 : 0;
-		if (nrOutputs > 1) out(1, 0) = (std::get<4>(record) == "Iris-versicolor") ? 1 : 0;
-		if (nrOutputs > 2) out(2, 0) = (std::get<4>(record) == "Iris-virginica") ? 1 : 0;
-
-		Eigen::VectorXd res = neuralNetwork.Predict(in.col(0));
-
-
-		setosaStats.AddPrediction(res(0) > 0.5, out(0, 0) > 0.5);
-		if (nrOutputs > 1) versicolorStats.AddPrediction(res(1) > 0.5, out(1, 0) > 0.5);
-		if (nrOutputs > 2) virginicaStats.AddPrediction(res(2) > 0.5, out(2, 0) > 0.5);
-
-		double limp = 0.5;
-		for (int j = 0; j < nrOutputs; ++j)
-			limp = std::max(limp, res(j));
-
-		if (res(0) == limp && out(0, 0) > 0.5) ++correct;
-		else if (nrOutputs > 1 && res(1) == limp && out(1, 0) > 0.5) ++correct;
-		else if (nrOutputs > 2 && res(2) == limp && out(2, 0) > 0.5) ++correct;
-	}
-
-	Utils::TestStatistics totalStats;
-
-	setosaStats.PrintStatistics("Setosa");
-	if (nrOutputs > 1) {
-		versicolorStats.PrintStatistics("Versicolor");
-		if (nrOutputs > 2) virginicaStats.PrintStatistics("Virginica");
-
-		totalStats.Add(setosaStats);
-		totalStats.Add(versicolorStats);
-		if (nrOutputs > 2) totalStats.Add(virginicaStats);
-
-		//totalStats.PrintStatistics("Overall"); //misleading
-	}
-
-
-	std::cout << "Accuracy (% correct): " << 100.0 * static_cast<double>(correct) / static_cast<double>(trainingSet.size()) << "%" << std::endl << std::endl;
-
-	setosaStats.Clear();
-	versicolorStats.Clear();
-	virginicaStats.Clear();
+	PrintStats(neuralNetwork, trainingSet, batchSize, nrOutputs);
 
 	std::cout << std::endl << "Test set:" << std::endl;
 
-	correct = 0;
-
-	for (const auto& record : testSet)
-	{
-		in(0, 0) = std::get<0>(record);
-		in(1, 0) = std::get<1>(record);
-		in(2, 0) = std::get<2>(record);
-		in(3, 0) = std::get<3>(record);
-
-		out(0, 0) = (std::get<4>(record) == "Iris-setosa") ? 1 : 0;
-		if (nrOutputs > 1) out(1, 0) = (std::get<4>(record) == "Iris-versicolor") ? 1 : 0;
-		if (nrOutputs > 2) out(2, 0) = (std::get<4>(record) == "Iris-virginica") ? 1 : 0;
-
-		Eigen::VectorXd res = neuralNetwork.Predict(in.col(0));
-		setosaStats.AddPrediction(res(0) > 0.5, out(0, 0) > 0.5);
-		if (nrOutputs > 1) versicolorStats.AddPrediction(res(1) > 0.5, out(1, 0) > 0.5);
-		if (nrOutputs > 2) virginicaStats.AddPrediction(res(2) > 0.5, out(2, 0) > 0.5);
-
-		double limp = 0.5;
-		for (int j = 0; j < nrOutputs; ++j)
-			limp = std::max(limp, res(j));
-
-		if (res(0) == limp && out(0, 0) > 0.5) ++correct;
-		else if (nrOutputs > 1 && res(1) == limp && out(1, 0) > 0.5) ++correct;
-		else if (nrOutputs > 2 && res(2) == limp && out(2, 0) > 0.5) ++correct;
-	}
-
-	setosaStats.PrintStatistics("Setosa");
-	if (nrOutputs > 1)
-	{
-		versicolorStats.PrintStatistics("Versicolor");
-		if (nrOutputs > 2) virginicaStats.PrintStatistics("Virginica");
-
-		totalStats.Clear();
-		totalStats.Add(setosaStats);
-		totalStats.Add(versicolorStats);
-		if (nrOutputs > 2) totalStats.Add(virginicaStats);
-
-		//totalStats.PrintStatistics("Overall"); //misleading
-	}
-
-	std::cout << "Accuracy (% correct): " << 100.0 * static_cast<double>(correct) / static_cast<double>(testSet.size()) << "%" << std::endl << std::endl;
+	PrintStats(neuralNetwork, testSet, batchSize, nrOutputs);
 
 	return true;
 }
