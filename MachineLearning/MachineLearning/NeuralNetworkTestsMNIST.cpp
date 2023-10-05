aa534120a13fbc47b4cf6deab7cfa7783f8e0285
Adrian Roman <aroman_ro@yahoo.com>
2023-10-05 14:09:31 +0300

    Still playing with hyperparameters

--- MachineLearning/MachineLearning/NeuralNetworkTestsMNIST.cpp
+++ MachineLearning/MachineLearning/NeuralNetworkTestsMNIST.cpp
@@ -146,7 +146,7 @@ bool NeuralNetworkTestsMNIST()
 	double alpha = 0.0015; // non const, so it can be adjusted
 	double decay = 0.95;
 	const double beta1 = 0.9;
-	const double beta2 = 0.97;
+	const double beta2 = 0.98;
 	const double lim = 10;
 	double lambda = 0.001;
 
